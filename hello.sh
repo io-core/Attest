4f4225f0fced0432c487d8d991d066b15e07a0dc
Charles Perkins <chuck@kuracali.com>
2018-12-01 11:58:26 -0800

    adapt ifaok shell script to pass through parameters

--- hello.sh
+++ hello.sh
@@ -1,14 +1,16 @@
-echo "Hello from the shell script."
+echo "Hello from the shell script..."
+echo "$1"
 exit 0 
+
 
 # ----Attest-0.1.0------------------------------------------------------------------------ #
 #  signed                                                                                  #
-#  2018-12-01 11:33:45                                                                     #
+#  2018-12-01 11:56:55                                                                     #
 # ---------------------------------------------------------------------------------------- #
-#  uRp7F8ULv7aQ6LpvfmIfPUcAPkdwBwUdh3S08p/tC1+swuqV1q1jT03XjbKeKYsd6cuASUJwK2C16VzEAkjTNW  #
-#  h/6OurMATAqvZUbzhj4tgLu0B1CQwosk08Ak/eAEb4EDi7VjgzzdCZ7z+FouGOiXS1jBxsAKGHJeg7nO3GR09S  #
-#  syROEarAFjL3LKw5VS3ak917wgDGHni/XYCjezcnTJEU6UvDlOo0F42RVAVLxY+8zQutki/mmBD+lKky2ir4Jt  #
-#  an7pW7V122e+3bUI5NftCtYarhy1VHdgIGh3AYUDSvlVFmD4S69P/nY+1u/gEJ3TH672asYdRVK+jU2/E5Ig==  #
+#  B+bqhCmMesKIytlAX8HtmcMW1ZFMGaDdKZ1Hx+jfj/3q12pqrZZ/Lz4A+c/HxXg4v1/jLEb8XngqLUNpRa7pLJ  #
+#  H4q7rjBuS2ikPJKJZ6Yp2fhiQ7V6/8ckJvenHwcNKBAvnC5iGBwzeZlM0cJApLmYliGPnOmiCLan14qcKb5aZu  #
+#  7ckYaDyZzWJf5L9JJ8I6oVrTgLqN8sEQyQ2774mAvsskuOrHv2sUkXUyXnB0EkZUZ3hBmaKWOeD0FRUTA1wkkI  #
+#  Lv0111AfgP+Cmif50a2V3rCsy0l1NkpObI0DNeNWXbLYu6d+lBJioawPnoxKSOJEXhkwdrBruhwav7MNRbHw==  #
 # ---------------------------------------------------------------------------------------- #
 #  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDsrtAUhLbs/ELXgH3OJs0SKh7tSQE/gkPavHv4//tsLucTAN  #
 #  C4mEjbjxKtFlZjji89GGvatnGu3DvAAz60VNEGBccezdn4rkcNpceKQe2KE2Kb13KM6VmrNl4Gj3+C278u0yKx  #
