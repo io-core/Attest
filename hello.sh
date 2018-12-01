94473f98de448a1b8defd12620556f917e344ed8
Charles Perkins <chuck@kuracali.com>
2018-12-01 11:46:31 -0800

    add example bash check script

---
+++ hello.sh
@@ -0,0 +1,18 @@
+echo "Hello from the shell script."
+exit 0 
+
+# ----Attest-0.1.0------------------------------------------------------------------------ #
+#  signed                                                                                  #
+#  2018-12-01 11:33:45                                                                     #
+# ---------------------------------------------------------------------------------------- #
+#  uRp7F8ULv7aQ6LpvfmIfPUcAPkdwBwUdh3S08p/tC1+swuqV1q1jT03XjbKeKYsd6cuASUJwK2C16VzEAkjTNW  #
+#  h/6OurMATAqvZUbzhj4tgLu0B1CQwosk08Ak/eAEb4EDi7VjgzzdCZ7z+FouGOiXS1jBxsAKGHJeg7nO3GR09S  #
+#  syROEarAFjL3LKw5VS3ak917wgDGHni/XYCjezcnTJEU6UvDlOo0F42RVAVLxY+8zQutki/mmBD+lKky2ir4Jt  #
+#  an7pW7V122e+3bUI5NftCtYarhy1VHdgIGh3AYUDSvlVFmD4S69P/nY+1u/gEJ3TH672asYdRVK+jU2/E5Ig==  #
+# ---------------------------------------------------------------------------------------- #
+#  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDsrtAUhLbs/ELXgH3OJs0SKh7tSQE/gkPavHv4//tsLucTAN  #
+#  C4mEjbjxKtFlZjji89GGvatnGu3DvAAz60VNEGBccezdn4rkcNpceKQe2KE2Kb13KM6VmrNl4Gj3+C278u0yKx  #
+#  l07WpQCYJ1x6WU8Tnrs5oRSGvHzJVvkxbH7YfymnoXbDg2j8cWYX+zNR/aYvcX+6isZmqRDg+qZ1CK45UL0sO9  #
+#  GcSFyey3fGigzWGvBx9JujvsxL6aqX7yY+WtCbApeGLN4HYtrn4ueuKAQND5EYo0SEI2m+STt5eCdDBLFhG0jD  #
+#  5MO6T7o//Mg8qDeuiY5wpbcQdpVWmdWQQxMT chuck@kuracali.com                                 #
+# ---------------------------------------------------------------------------------------- #
